0a35268fe8c79fbf559f3140e3b3ba8407f7a398
Graziella Layouss <graziella@Graziellas-MBP.fios-router.home>
2017-03-06 04:06:36 -0500

    clean directory

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup
 setup(name='flavors',
       version='0.1',
       description='Flavor Library',
-      url='',
+      keywords='flavors wheel tasting',
+      url='https://github.com/gtoutoungis1/flavors',
       author='Graziella Toutoungis',
       author_email='graziella.toutoungis@gmail.com',
       license='',
       packages=['flavors'],
       install_requires=[
           'fuzzywuzzy',
+          'python-Levenshtein'
       ],
       zip_safe=False)
